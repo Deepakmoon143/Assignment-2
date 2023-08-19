9ff2df3c4a1f2da70f053d5e0b9d958fe9bf9382
Deepak <53943221+Deepakmoon143@users.noreply.github.com>
2023-08-19 18:01:23 +0530

    Question_2.py

--- Question_2.py
+++ Question_2.py
@@ -1,10 +1,3 @@
-'''list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-
-sort = sorted(list, key=lambda x: x[-1])
-
-print(sort)
-'''
-
 list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 sort = sorted (list, key=lambda x: x[-1] )
 print (sort)

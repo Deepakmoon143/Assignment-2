2746719872beaa283b291e97a46b828d28f43d68
Deepak <53943221+Deepakmoon143@users.noreply.github.com>
2023-08-22 02:39:02 +0530

    Question_1.py

--- Question_1.py
+++ Question_1.py
@@ -1,9 +1,8 @@
-user = int(input("enter the number of tuples: "))
+user = int(input("enter the number of tuples"))
 list= []
 for i in range(user):
-    tuple_input = input("enter the tuple: ").split(",")
+    tuple_input = input("enter the tuple").split(",")
     tuple_convert= tuple(map(int,tuple_input))
     list.append(tuple_convert)
 sort_list= sorted(list,key= lambda x: x[-1])
-for j in sort_list:
-    print(j)
+print(sort_list)

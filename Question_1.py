e9e451b1649407f035024f86a8a7fdfd9f24ae10
Deepak <53943221+Deepakmoon143@users.noreply.github.com>
2023-08-22 02:30:55 +0530

    Question_1.py

--- Question_1.py
+++ Question_1.py
@@ -1,6 +1,9 @@
-letter_to_ascii = {} 
-
-for i in range(ord('a'), ord('z') + 1):
-    letter_to_ascii[chr(i)] = i
-
-print(letter_to_ascii)
+user = int(input("enter the number of tuples: "))
+list= []
+for i in range(user):
+    tuple_input = input("enter the tuple: ").split(",")
+    tuple_convert= tuple(map(int,tuple_input))
+    list.append(tuple_convert)
+sort_list= sorted(list,key= lambda x: x[-1])
+for j in sort_list:
+    print(j)

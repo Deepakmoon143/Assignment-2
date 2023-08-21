032a98173941c1b3dcb9763672fda3279202197e
Deepak <53943221+Deepakmoon143@users.noreply.github.com>
2023-08-22 02:51:40 +0530

    Question_2.py

--- Question_2.py
+++ Question_2.py
@@ -1,6 +1,6 @@
-user1 = input("enter the first alphabet from a-z")
-user2 = input("enter the last alphabet from a-z")
+user_input1 = input("enter the starting ASCII values: ")
+user_input2 = input("enter the ending ASCII values: ")
 text = {}
-for i in range(ord(user1), ord(user2)+1):
+for i in range(ord(user_input1), ord(user_input2)+1):
     text [chr(i)] = i
 print(text)
